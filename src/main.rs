242deb31cd9a2d0a7f57de2444fd0b48694a7605
Mathspy <mathspy257@gmail.com>
2022-01-22 13:20:42 +0200

    Parse target platform
    Add support for only Windows64 right now

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use std::{
 };
 
 #[derive(Debug)]
+enum Endianess {
+    Big,
+    Little,
+}
+
+#[derive(Debug)]
 struct Header {
     version: u32,
-    endianess: u8,
+    endianess: Endianess,
     // reserved??
     metadata: u32,
     file_size: u64,
@@ -27,7 +33,11 @@ fn get_header<R: BufRead>(file: &mut R) -> Result<Header> {
     file.read_exact(&mut buffer[0..4])?;
 
     file.read_exact(&mut buffer[0..1])?;
-    let endianess = buffer[0];
+    let endianess = if buffer[0] == 0 {
+        Endianess::Little
+    } else {
+        Endianess::Big
+    };
 
     // Throw away "reserved" for now
     file.read_exact(&mut buffer[0..3])?;
@@ -68,12 +78,40 @@ fn get_unity_version<R: BufRead>(file: &mut R) -> Result<String> {
     String::from_utf8(buffer).context("Failed to parse unity version as valid utf-8")
 }
 
+#[derive(Debug)]
+enum TargetPlatform {
+    Unknown(i32),
+    Windows64,
+}
+
+impl From<i32> for TargetPlatform {
+    fn from(value: i32) -> Self {
+        match value {
+            19 => TargetPlatform::Windows64,
+            value => TargetPlatform::Unknown(value),
+        }
+    }
+}
+
+fn get_target_platform<R: BufRead>(file: &mut R, endianess: Endianess) -> Result<TargetPlatform> {
+    let mut buffer = [0u8; 4];
+
+    file.read_exact(&mut buffer)?;
+    let target_platform = match endianess {
+        Endianess::Big => i32::from_be_bytes(buffer),
+        Endianess::Little => i32::from_le_bytes(buffer),
+    };
+
+    Ok(target_platform.into())
+}
+
 fn main() -> Result<()> {
     let file = File::open("/Users/mathspy/Downloads/resources.assets")?;
     let mut file = BufReader::new(file);
 
-    dbg!(get_header(&mut file)?);
+    let header = dbg!(get_header(&mut file)?);
     dbg!(get_unity_version(&mut file)?);
+    dbg!(get_target_platform(&mut file, header.endianess)?);
 
     Ok(())
 }
