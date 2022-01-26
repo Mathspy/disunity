f1741b9e8adc2e4bbdd6174a2189821b64a4884e
Mathspy <mathspy257@gmail.com>
2022-01-26 22:14:11 +0200

    Derive macro: Clarify test names
    Now that we have both an enum attribute and a variant attribute it's
    important that the tests make this distinction clear

--- disunity-derive/tests/mod.rs
+++ disunity-derive/tests/mod.rs
@@ -1,16 +1,16 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/01-fails-non-enum.rs");
+    t.compile_fail("tests/01-non-enum.rs");
     t.pass("tests/02-simple.rs");
     t.pass("tests/03-with-fields.rs");
     t.compile_fail("tests/04-ignore-unknown-field.rs");
-    t.compile_fail("tests/05-invalid-discriminant.rs");
-    t.compile_fail("tests/06-too-many-attributes.rs");
-    t.compile_fail("tests/07-missing-disunity-attribute.rs");
-    t.compile_fail("tests/08-missing-discriminant-val.rs");
-    t.compile_fail("tests/09-random-literal-in-attribute.rs");
-    t.compile_fail("tests/10-misspell-attribute-list.rs");
+    t.compile_fail("tests/05-invalid-variant-discriminant.rs");
+    t.compile_fail("tests/06-too-many-variant-attributes.rs");
+    t.compile_fail("tests/07-missing-variant-discriminant.rs");
+    t.compile_fail("tests/08-missing-variant-discriminant-val.rs");
+    t.compile_fail("tests/09-random-literal-in-variant-attribute.rs");
+    t.compile_fail("tests/10-misspelled-variant-attribute.rs");
     t.pass("tests/11-from-integer.rs");
     t.pass("tests/12-from-variant.rs");
     t.pass("tests/13-discriminant-type.rs");
